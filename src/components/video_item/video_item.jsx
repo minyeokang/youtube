c69572091f76e63fa2d05b87fcb4ef80c7c2b1ff
minyeo kang <dobieborie1@gmail.com>
2022-04-23 18:14:27 +0900

    search function

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from './video_item.module.css';
 
-const VideoItem = ({ video: {snippet}}) => <li className={styles.container}>
+const VideoItem = ({ video: {snippet}}) => 
+<li className={styles.container}>
     <div className={styles.video}>
     <img className={styles.thumbnail} src={snippet.thumbnails.medium.url} alt="video thumbnail" />
     <div className={styles.metadata}>
